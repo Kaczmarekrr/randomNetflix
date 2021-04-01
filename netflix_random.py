8f46d8c7b65722176944171e82811d46f3ef3ac0
Kaczmarekrr <62336599+Kaczmarekrr@users.noreply.github.com>
2021-04-01 17:49:39 +0200

    Add files via upload
    Ver 0.1

---
+++ netflix_random.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 19:24:13 2021
+
+@author: kaczm
+"""
+
+import numpy as np
+import pandas as pd
+import time
+
+# directkeys.py
+# http://stackoverflow.com/questions/13564851/generate-keyboard-events
+# msdn.microsoft.com/en-us/library/dd375731
+
+import ctypes
+from ctypes import wintypes
+import time
+
+user32 = ctypes.WinDLL('user32', use_last_error=True)
+
+INPUT_MOUSE    = 0
+INPUT_KEYBOARD = 1
+INPUT_HARDWARE = 2
+
+KEYEVENTF_EXTENDEDKEY = 0x0001
+KEYEVENTF_KEYUP       = 0x0002
+KEYEVENTF_UNICODE     = 0x0004
+KEYEVENTF_SCANCODE    = 0x0008
+
+MAPVK_VK_TO_VSC = 0
+
+# List of all codes for keys:
+# # msdn.microsoft.com/en-us/library/dd375731
+UP = 0x26
+DOWN = 0x28
+A = 0x41
+
+# C struct definitions
+
+wintypes.ULONG_PTR = wintypes.WPARAM
+
+class MOUSEINPUT(ctypes.Structure):
+    _fields_ = (("dx",          wintypes.LONG),
+                ("dy",          wintypes.LONG),
+                ("mouseData",   wintypes.DWORD),
+                ("dwFlags",     wintypes.DWORD),
+                ("time",        wintypes.DWORD),
+                ("dwExtraInfo", wintypes.ULONG_PTR))
+
+class KEYBDINPUT(ctypes.Structure):
+    _fields_ = (("wVk",         wintypes.WORD),
+                ("wScan",       wintypes.WORD),
+                ("dwFlags",     wintypes.DWORD),
+                ("time",        wintypes.DWORD),
+                ("dwExtraInfo", wintypes.ULONG_PTR))
+
+    def __init__(self, *args, **kwds):
+        super(KEYBDINPUT, self).__init__(*args, **kwds)
+        # some programs use the scan code even if KEYEVENTF_SCANCODE
+        # isn't set in dwFflags, so attempt to map the correct code.
+        if not self.dwFlags & KEYEVENTF_UNICODE:
+            self.wScan = user32.MapVirtualKeyExW(self.wVk,
+                                                 MAPVK_VK_TO_VSC, 0)
+
+class HARDWAREINPUT(ctypes.Structure):
+    _fields_ = (("uMsg",    wintypes.DWORD),
+                ("wParamL", wintypes.WORD),
+                ("wParamH", wintypes.WORD))
+
+class INPUT(ctypes.Structure):
+    class _INPUT(ctypes.Union):
+        _fields_ = (("ki", KEYBDINPUT),
+                    ("mi", MOUSEINPUT),
+                    ("hi", HARDWAREINPUT))
+    _anonymous_ = ("_input",)
+    _fields_ = (("type",   wintypes.DWORD),
+                ("_input", _INPUT))
+
+LPINPUT = ctypes.POINTER(INPUT)
+
+def _check_count(result, func, args):
+    if result == 0:
+        raise ctypes.WinError(ctypes.get_last_error())
+    return args
+
+user32.SendInput.errcheck = _check_count
+user32.SendInput.argtypes = (wintypes.UINT, # nInputs
+                             LPINPUT,       # pInputs
+                             ctypes.c_int)  # cbSize
+
+# Functions
+
+def PressKey(hexKeyCode):
+    x = INPUT(type=INPUT_KEYBOARD,
+              ki=KEYBDINPUT(wVk=hexKeyCode))
+    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    x = INPUT(type=INPUT_KEYBOARD,
+              ki=KEYBDINPUT(wVk=hexKeyCode,
+                            dwFlags=KEYEVENTF_KEYUP))
+    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+    
+import subprocess
+
+def copy2clip(txt):
+    cmd='echo '+txt.strip()+'|clip'
+    return subprocess.check_call(cmd, shell=True)
+    
+    
+netflix_df = pd.read_csv("netflix_titles.csv")
+
+typ = ["Horror Movies", "International Movies"]
+
+
+filtered_data = netflix_df.loc[
+    lambda netflix_df: netflix_df.listed_in.apply(
+        lambda l: 'Dramas' in l
+    )
+]
+#print(filtered_data)
+
+
+lista = filtered_data.values.tolist()
+
+
+random_num = np.random.randint(0,len(lista))
+random_title = lista[random_num][2]
+random_type = lista[random_num][1]
+print(random_title,random_type)
+
+
+
+name = random_title
+copy2clip(name)
+print(f'twój {random_type} to', name)
+time.sleep(3)
+print('wklejam!')
+PressKey(0x11)
+PressKey(0x56)
+time.sleep(0.1)
+ReleaseKey(0x11)
+ReleaseKey(0x56)
+PressKey(0x0D)
+ReleaseKey(0x0D)
+
+print(f"Miłego seansu!")
+
+
+
